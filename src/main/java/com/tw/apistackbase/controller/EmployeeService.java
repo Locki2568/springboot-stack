ae1457a06a04adf8ff082f573d47aae7fffa62bc
Locki Lo <locki.lo@cargosmart.com>
2018-11-27 18:09:53 +0800

    fork

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class EmployeeService {
+    private List employees;
+    public EmployeeService(){
+        employees = new ArrayList();
+        Employees employees1 = new Employees(0,"Xiaoming", 20, "Male");
+        Employees employees2 = new Employees(1,"Xiaohong", 19, "Female");
+        Employees employees3 = new Employees(2,"Xiaozhi", 15, "Male");
+        Employees employees4 = new Employees(3,"Xiaogang", 16, "Male");
+        Employees employees5 = new Employees(4,"Xiaoxia", 15, "Female");
+        employees.add(employees1);
+        employees.add(employees2);
+        employees.add(employees3);
+        employees.add(employees4);
+        employees.add(employees5);
+    }
+
+    public List<Employees> getAll(){
+        return this.employees;
+    }
+
+    public Employees searchEmployee(int id){
+        Employees employees = (Employees) this.employees.get(id);
+        return employees;
+    }
+
+}
---
+++ src/main/java/com/tw/apistackbase/controller/EmployeesResource.java
@@ -0,0 +1,43 @@
+package com.tw.apistackbase.controller;
+
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by jxzhong on 18/08/2017.
+ */
+@RestController
+@RequestMapping("/employees")
+public class EmployeesResource {
+
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+    EmployeeService employeeService = new EmployeeService();
+
+    @Autowired
+    public EmployeesResource(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(produces = {"application/json"})
+    public List<Employees> getAll() {
+        return employeeService.getAll();
+    }
+
+    @PostMapping(path = "/{id}",produces = {"application/json"})
+    public Employees search(@RequestBody Employees employees){
+        int num = id;
+        employeeService.searchEmployee(id);
+
+    return id;
+    }
+
+}
